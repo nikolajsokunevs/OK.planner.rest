b81cfaf7b163c52f3bef8a9229b9e771bf813d85
mhakimov <xakim@inbox.lv>
2019-05-18 10:13:37 +0100

    raw email func

--- src/main/java/lv/ok/service/UserServiceImpl.java
+++ src/main/java/lv/ok/service/UserServiceImpl.java
@@ -7,8 +7,16 @@ import lv.ok.models.User;
 import lv.ok.repository.UserRepository;
 import lv.ok.resources.responses.LoginResponse;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Date;
+import java.util.Properties;
 
 public class UserServiceImpl implements IUserService {
 
@@ -24,6 +32,48 @@ public class UserServiceImpl implements IUserService {
         else {
             user.setDateCreated();
             userRepository.insertUser(user);
+
+            try {
+                String host = "smtp.gmail.com";
+                String user2 = "mark.gusman11@gmail.com";
+                String from = "mark.gusman11@gmail.com";
+                String to = user.getUsername();
+
+//                String user2 = "xakim@inbox.lv";
+//                String from = "xakim@inbox.lv";
+//                String host = "mail.inbox.lv";
+
+
+                String database = "planitnow";
+
+                Properties properties = System.getProperties();
+                properties.put("mail.smtp.starttls.enable", "true");
+                properties.put("mail.smtp.host", host);
+                properties.put("mail.smtp.port", "587");
+                properties.put("mail.smtp.auth", "true");
+                properties.put("mail.smtp.starttls.required", "true");
+
+                java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+
+                Session mailSession = Session.getDefaultInstance(properties, null);
+                mailSession.setDebug(false);
+                Message msg = new MimeMessage(mailSession);
+                msg.setFrom(new InternetAddress(from));
+                InternetAddress[] address = {new InternetAddress(to)};
+                msg.setRecipients(Message.RecipientType.TO, address);
+                msg.setSubject("welcome to PlanIt");
+                msg.setSentDate(new Date());
+                msg.setText("This is email text");
+                Transport transport = mailSession.getTransport("smtp");
+                transport.connect(host, user2, database);
+                transport.sendMessage(msg, msg.getAllRecipients());
+                transport.close();
+                System.out.println("email sent successfully");
+            }
+            catch (MessagingException mex) {
+                mex.printStackTrace();
+            }
+            
             return "Username " + user.getUsername() + " has been added successfully.";
         }
     }
